c9c35f0fd5d3a13d09b7ceffb14ffb139d39f3ef
rohitrdongare <rohitdongare1999@gmail.com>
2024-01-14 09:46:56 +0530

    Installation

---
+++ Introduction/sellIntroduction.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+public class sellIntroduction {
+
+	public static void main(String[] args) {
+
+		
+		
+		//class object name= new(is keyword) method()
+		
+		
+		//chromerdriver.exe  
+		//invoking Browser or sellenium manager
+		WebDriver driver= new ChromeDriver();
+		//ChromeDriver driver= new ChromeDriver()
+	}
+
+}
